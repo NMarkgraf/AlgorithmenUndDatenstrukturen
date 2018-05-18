dbcd5aff56f07591cf12d4d6ff9fe0d2c60f6245
Norman Markgraf <nmarkgraf@hotmail.com>
2018-05-18 18:15:49 +0200

    Die Quelltexte etwas pep8 und pep257 konformer gestaltet.

--- python/lists.py
+++ python/lists.py
@@ -1,12 +1,13 @@
 '''
+
 Algorithmen und Datenstrukturen in Python
 =========================================
 
 Implementierungen von Norman Markgraf aus dem Jahr 2018
 
-Listen. - Pyhton hat eigentlich eine optimierte Listenverwaltung. 
+Listen. - Pyhton hat eigentlich eine optimierte Listenverwaltung.
 
-Da es aber um die grundlegenden Algorithmen geht, 
+Da es aber um die grundlegenden Algorithmen geht,
 machen wir davon weitgehend keinen Gebrauch!
 
 *Node* ist unsere Klasse für unsere Datenknoten
@@ -17,59 +18,57 @@ machen wir davon weitgehend keinen Gebrauch!
 
 *Queue* für eine Schlange abgeleitet.
 
-
 '''
 
 from abc import ABC, abstractmethod
 
+
 class Node:
-    
-  _key = None
-  _next = None
 
-  def __init__(self):
-    self._key = None
-    self._next = None
+    _key = None
+    _next = None
 
-  def __repr__(self):
-      return repr(self._key)
-      
-  def __str__(self):
-      return str(self._key)
+    def __init__(self):
+        self._key = None
+        self._next = None
+
+    def __repr__(self):
+        return repr(self._key)
+
+    def __str__(self):
+        return str(self._key)
+
+# ============================================================================
 
 
 class AbstractLinearList(ABC):
 
-  _head = None
-  _tail = None
+    _head = None
+    _tail = None
 
-  @abstractmethod
-  def insertafter(self, node, value):
-      ''' insert new value after node
-      '''
-      pass
-  
-  @abstractmethod
-  def deletenext(self, node):
-      ''' delete next node
-      '''
-      pass
+    @abstractmethod
+    def insertafter(self, node, value):
+        """Insert new value after node."""
+        pass
 
-  @abstractmethod
-  def search(self, node, value):
-      ''' search for value starting at node
-      '''
-      pass
+    @abstractmethod
+    def deletenext(self, node):
+        """Delete next node."""
+        pass
 
-  @abstractmethod
-  def isempty(self):
-      ''' is list empty?
-      '''
-      pass
-  
+    @abstractmethod
+    def search(self, node, value):
+        """Search for value starting at node."""
+        pass
+
+    @abstractmethod
+    def isempty(self):
+        """Check if list is empty."""
+        pass
+
 # -----------------------------------------------------------------------
 class LinearList(AbstractLinearList):
-    
+
     def __init__(self):
         self._head = Node()
         self._tail = Node()
@@ -77,202 +76,204 @@ class LinearList(AbstractLinearList):
         self._head._key = -1
         self._tail._key = -1
         self._tail._next = self._tail
-    
+
     def __repr__(self):
         t = self._head._next
         rs = ""
         while (t._next != t):
             rs += repr(t) + " "
             t = t._next
-          
+
         return(rs)
-      
+
     def __str__(self):
         t = self._head._next
         rs = "["
         while (t._next != t):
             rs += str(t) + ", "
-            t = t._next 
-          
+            t = t._next
+
         rs += "]"
         return(rs)
-    
+
     def insertafter(self, node, value):
-      newNode = Node()
-      newNode._key = value
-      newNode._next = node._next
-      node._next = newNode
-      return newNode
-      
+        newNode = Node()
+        newNode._key = value
+        newNode._next = node._next
+        node._next = newNode
+        return newNode
+
     def deletenext(self, node):
-      t = node._next
-      v = node._next._key
-      node._next = node._next._next
-      if (t != self._tail):
-          del(t)
-      return v
-      
+        t = node._next
+        v = node._next._key
+        node._next = node._next._next
+        if (t != self._tail):
+            del(t)
+        return v
+
     def search(self, node, key):
-        ''' search
-           
+        """ search
+
            Suche mit Sentinaltechnik
-        '''
+        """
         self._tail._key = key
         while (node._key != key):
             node = node._next
         return node
-    
+
     def isempty(self):
         return self._head._next == self._tail
-    
+
     def istail(self, node):
         return node._next == node
-        
+
     def isinlist(self, key):
         node = self.search(self._head, key)
         return not self.istail(node)
- 
+
 # -----------------------------------------------------------------------
+
+
 class SortedLinearList(LinearList):
-    ''' Implementiert eine sortierte linearen Liste
-    '''
-    
+    """Implementiert eine sortierte linearen Liste."""
+
     def search(self, node, key):
         self._tail._key = key
         while (node._next._key < key):
             node = node._next
         return node
-    
+
     def insert(self, key):
         n = self.search(self._head, key)
         self.insertafter(n, key)
 
-
 # -----------------------------------------------------------------------
+
+
 class Stack(LinearList):
-    ''' Implementiert einen Sack mit Hilfe einer linearen Liste
-    '''
-    
+    """Implementiert einen Sack mit Hilfe einer linearen Liste."""
+
     def push(self, key):
         self.insertafter(self._head, key)
-        
+
     def pop(self):
         return self.deletenext(self._head)
 
-
 # -----------------------------------------------------------------------
+
+
 class Queue(LinearList):
-    ''' Implementiert eine Queue (Schlange) mit Hilfe einer linearen Liste
-    '''
-    
+    """ Implementiert eine Queue (Schlange) mit Hilfe einer linearen Liste."""
+
     def get(self):
         return self.deletenext(self._head)
-        
+
     def put(self, key):
         t = self._head
         while (t._next != self._tail):
             t = t._next
         self.insertafter(t, key)
- 
- 
+
 # -----------------------------------------------------------------------
+
+
 class Ring(LinearList):
-  ''' Implementiert einen Ring mit Hilfe einer linearen Liste
-  '''
-  
-  def __init__(self):
-    super().__init__()
-    self._head._next = self._head
-        
-  def __repr__(self):
-      rs = "("
-      n = self._head._next
-      while n._next != self._head._next:
-          rs += repr(n) + " "
-          n = n._next
-      rs += repr(n) + ")"
-      return rs
+    """Implementiert einen Ring mit Hilfe einer linearen Liste."""
 
-  def __str__(self):
-      rs = "("
-      n = self._head._next
-      while n._next != self._head._next:
-          rs += str(n) + ", "
-          n = n._next
-      rs += str(n) + ")"
-      return rs
+    def __init__(self):
+        super().__init__()
+        self._head._next = self._head
 
-  def isempty(self):
-    return self._head._next == self._head
+    def __repr__(self):
+        rs = "("
+        n = self._head._next
+        while n._next != self._head._next:
+            rs += repr(n) + " "
+            n = n._next
+        rs += repr(n) + ")"
+        return rs
 
-  def insert(self, key):
-    tail = self._head._next
-    while tail._next != self._head._next:
-        tail = tail._next
-    newnode = self.insertafter(tail, key)
-    if newnode._next == self._head:
-        newnode._next = newnode
-        
-  def searchnext(self, node, key):
-    while (node._next._key != key):
-        node = node._next
-        if node._next == self._head._next:
-            break
-    return node
+    def __str__(self):
+        rs = "("
+        n = self._head._next
+        while n._next != self._head._next:
+            rs += str(n) + ", "
+            n = n._next
+        rs += str(n) + ")"
+        return rs
 
-  def delete(self, key):
-      node = self.searchnext(self._head, key)
-      if node._next != self._head._next:
-          self.deletenext(node)
+    def isempty(self):
+        return self._head._next == self._head
 
-  def getnext(self, node):
-      return node._next
+    def insert(self, key):
+        tail = self._head._next
+        while tail._next != self._head._next:
+            tail = tail._next
+        newnode = self.insertafter(tail, key)
+        if newnode._next == self._head:
+            newnode._next = newnode
+
+    def searchnext(self, node, key):
+        while (node._next._key != key):
+            node = node._next
+            if node._next == self._head._next:
+                break
+        return node
+
+    def delete(self, key):
+        node = self.searchnext(self._head, key)
+        if node._next != self._head._next:
+            self.deletenext(node)
+
+    def getnext(self, node):
+        return node._next
 
 # ===========================================================================
+
+
 def main():
     # -----------------------------------------------------------------------
     print("Lineare Liste:")
     list = LinearList()
 
-    print(list) 
+    print(list)
     list.insertafter(list._head, 1)
-    print(list) 
+    print(list)
     nn = list.insertafter(list._head, 2)
-    print(list)    
+    print(list)
     list.insertafter(nn, 4)
-    print(list)    
+    print(list)
     nn = list.insertafter(nn, 42)
-    print(list)    
+    print(list)
     list.insertafter(nn, 4711)
     print(list)
 
     # -----------------------------------------------------------------------
     print("Sortierte lineare Liste:")
     list = SortedLinearList()
-    print(list) 
+    print(list)
     list.insert(4711)
     print(list)
     list.insert(1)
-    print(list) 
+    print(list)
     list.insert(4)
-    print(list)    
+    print(list)
     list.insert(2)
-    print(list)    
+    print(list)
     list.insert(42)
-    print(list)    
-    
+    print(list)
 
     # -----------------------------------------------------------------------
     print("Stack:")
     stack = Stack()
-    
+
     stack.push(1)
     stack.push(2)
     stack.push(3)
-    
+
     while not stack.isempty():
         print(stack.pop())
-    
+
     # -----------------------------------------------------------------------
     print("Queue:")
     queue = Queue()
@@ -287,7 +288,7 @@ def main():
     # -----------------------------------------------------------------------
     print("Ring:")
     ring = Ring()
-    
+
     ring.insert(1)
     ring.insert(2)
     ring.insert(3)
@@ -296,10 +297,11 @@ def main():
     print(ring)
 
     ring.delete(2)
-    
+
     print("- - - -")
 
     print(ring)
+
 
 if __name__ == "__main__":
     main()
--- python/tree.py
+++ python/tree.py
@@ -1,4 +1,5 @@
 '''
+
 Algorithmen und Datenstrukturen in Python
 =========================================
 
@@ -8,232 +9,312 @@ Binärer Such-Baum
 
 '''
 
+# ============================================================================
+
+
 class Node:
+    """Tree Node.
 
-  _key = None
-  _left = None
-  _right = None
-  _info = None
+    Ein einfacher Baum-Knoten. Er besteht aus einem Schlüssel `_key`, einem
+    linken und einem rechten Kindknoten `_left` und `_right`, sowie einem
+    Informationsspeicher `_info` in dem weitere Daten gespeichert werden
+    können.
+    """
 
-  def getLeft(self):
-    return self._left
+    _key = None  # Schlüssel
+    _left = None  # Linker Kindknoten
+    _right = None  # Rechter Kindknoten
+    _info = None  # Information(sspeicher)
 
-  def getRight(self):
-    return self._right
-    
-  def __lt__(self, a, b):
-      return a._key < b.key
+    def getLeft(self):
+        return self._left
 
-  def __le__(self, a, b):
-      return a._key <= b.key
+    def getRight(self):
+        return self._right
 
-  def __eq__(self, a, b):
-      return a._key == b._key
+    def getInfo(self):
+        return self._info
 
-  def __gt__(self, a, b):
-      return a._key > b.key
+    def __lt__(self, a, b):
+        return a._key < b.key
 
-  def __ge__(self, a, b):
-      return a._key >= b.key
+    def __le__(self, a, b):
+        return a._key <= b.key
 
-  def __ne__(self, a, b):
-      return a._key != b._key
+    def __eq__(self, a, b):
+        return a._key == b._key
 
-  def __str__(self):
-      return str(self._key)
+    def __gt__(self, a, b):
+        return a._key > b.key
+
+    def __ge__(self, a, b):
+        return a._key >= b.key
+
+    def __ne__(self, a, b):
+        return a._key != b._key
+
+    def __repr__(self):
+        return "("+repr(self._key)+": "+repr(self._info)+")"
+
+    def __str__(self):
+        return "("+str(self._key)+": "+str(self._info)+")"
+
+# ============================================================================
+
 
 class Tree:
+    """Binary tree.
 
-  _head = None
-  _sentinal = None
-  
-  def __init__(self):
-    self._head = Node()
-    self._sentinal = Node()
-    self._sentinal.key = -1
-    self._sentinal._left = self._sentinal
-    self._sentinal._right = self._sentinal
-    self._head._right = self._sentinal
-    self._head._key = -1
-    
-  def insert(self, key, info):
-    p = self._head
-    x = self._head._right
-    while x is not self._sentinal:
-      p = x;
-      if (key < x._key):
-        x = x._left
-      else:
-        x = x._right
-    x = Node()
-    x._key = key
-    x._info = info
-    x._left = self._sentinal
-    x._right = self._sentinal
-    if key < p._key:
-      p._left = x
-    else:
-      p._right = x
+    Ein binärer Baum mit Sentialtechnik.
+    """
 
-  def search(self, key):
-    x = self._head._right
+    _head = None  # Start Knoten, aber KEINE echteWurzel
+    _sentinal = None  # Sentinal als Ersatz für ein `None`
 
-    self._sentinal._key = key
+    def __init__(self):
+        """Initialise binary tree.
 
-    while (key != x._key):
-      if (key < x._key):
-        x = x._left
-      else:
-        x = x._right
-    return x._info
-    
-  def inorderRecursiv(self, node):
-    if node is not self._sentinal:
-      self.inorderRecursiv(node._left)
-      self.printnode(node)
-      self.inorderRecursiv(node._right)
+        Erzeuge einen neuen binären Baum.
+        """
+        self._head = Node()
+        self._sentinal = Node()
+        self._sentinal.key = -1
+        self._sentinal._left = self._sentinal
+        self._sentinal._right = self._sentinal
+        self._head._right = self._sentinal
+        self._head._key = -1
 
-  def preorderRecursiv(self, node):
-    if node is not self._sentinal:
-      self.printnode(node)
-      self.preorderRecursiv(node._left)
-      self.preorderRecursiv(node._right)
+    def insert(self, key, info):
+        """Insert a new node with key and infoself.
 
-  def postorderRecursiv(self, node):
-    if node is not self._sentinal:
-      self.postorderRecursiv(node._left)
-      self.postorderRecursiv(node._right)
-      self.printnode(node)
+        Füge einen neinen Knoten mit Schlüssel (key) und Information (info)
+        ein.
+        """
+        p = self._head
+        x = self._head._right
+        while x is not self._sentinal:
+            p = x
+            if (key < x._key):
+                x = x._left
+            else:
+                x = x._right
+        x = Node()
+        x._key = key
+        x._info = info
+        x._left = self._sentinal
+        x._right = self._sentinal
+        if key < p._key:
+            p._left = x
+        else:
+            p._right = x
 
-  def preorderIterativ(self):
-      stack = []  # Wir benutzen einmal den Python eigenen Stack, eine Liste. ;-)
-      
-      stack.append(self._head._right)
-      
-      while stack:
-          n = stack.pop()
-          self.printnode(n)
-          if n._right is not self._sentinal:
-              stack.append(n._right)
-          if n._left is not self._sentinal:
-              stack.append(n._left)
+    def __search(self, key):
+        # Wir starten hinter derm Kopfknoten an der Wurzel
+        x = self._head._right
 
+        # Sentinaltechnik! Wir setzen den Sential auf den Suchwert!
+        self._sentinal._key = key
 
-  def inorderIterativ(self):
-      stack = []  # Wir benutzen einmal den Python eigenen Stack, eine Liste. ;-)
-      
-      n = self._head._right
+        # Suchen bis der Suchwert gefunden wird.
+        while (key != x._key):  # Dank Sentinaltechnik werden wir finden!
+            if (key < x._key):
+                x = x._left
+            else:
+                x = x._right
+        return x
 
-      while (n is not self._sentinal):
-          stack.append(n)
-          n = n._left
-      
-      while stack:
-          n = stack.pop()
-          self.printnode(n)
-          n = n._right
-          while (n is not self._sentinal):
-              stack.append(n)
-              n = n._left
+    def search(self, key):
+        """Search in the tree.
 
-  def postorderIterativ(self):
-      stack = []  # Wir benutzen einmal den Python eigenen Stack, eine Liste. ;-)
-      
-      n = self._head._right
+        Suchen im Baum. Wir liefern `None` zurück, wenn wir der Schlüssel `key`
+        nicht im Baum finden. Sonst wir Information`_info` zurückgeliefert.
 
-      done = False
-      
-      while not done:
-        
-        while (n is not self._sentinal):
+        :param: key Suchschlüssel
+        """
+        x = self.__search(key)
+        return x if x is not self._sentinal else None
+
+    def preorderRecursiv(self, node):
+        """Recursive preorder traverse of a tree.
+
+        Preorder Traversierung des Baumes mittels Rekursion.
+        """
+        if node is not self._sentinal:
+            self.printnode(node)
+            self.preorderRecursiv(node._left)
+            self.preorderRecursiv(node._right)
+
+    def inorderRecursiv(self, node):
+        """Recursive inorder traverse of the tree.
+
+        Inorder Traversierung des Baumes mittels Rekursion.
+        """
+        if node is not self._sentinal:
+            self.inorderRecursiv(node._left)
+            self.printnode(node)
+            self.inorderRecursiv(node._right)
+
+    def postorderRecursiv(self, node):
+        """Recursive postorder traverse of the tree.
+
+        Postorder Traversierung des Baumes mittels Rekursion.
+        """
+        if node is not self._sentinal:
+            self.postorderRecursiv(node._left)
+            self.postorderRecursiv(node._right)
+            self.printnode(node)
+
+    def preorderIterativ(self):
+        """Traverse tree in preorder iterative.
+
+        Preorder Traversierung des Baumes mittels Iteration und eines Stacks.
+        """
+        stack = []  # Wir benutzen den Python eigenen Stack: eine Liste.
+
+        stack.append(self._head._right)
+
+        while stack:
+            n = stack.pop()
+            self.printnode(n)
             if n._right is not self._sentinal:
                 stack.append(n._right)
+            if n._left is not self._sentinal:
+                stack.append(n._left)
+
+    def inorderIterativ(self):
+        """Traverse tree in inorder iterative.
+
+        Inorder Traversierung des Baumes mittels Iteration und eines Stacks.
+        """
+        stack = []  # Wir benutzen den Python eigenen Stack: eine Liste.
+
+        n = self._head._right
+
+        while (n is not self._sentinal):
             stack.append(n)
             n = n._left
-        
-        n = stack.pop()
-        
-        if n._right is not self._sentinal and stack and stack[-1] is n._right:
-            stack.pop()
-            stack.append(n)
-            n = n._right
-        else:
+
+        while stack:
+            n = stack.pop()
             self.printnode(n)
-            n = self._sentinal
-        if not stack:
-            done = True
+            n = n._right
+            while (n is not self._sentinal):
+                stack.append(n)
+                n = n._left
 
+    def postorderIterativ(self):
+        """Traverse tree in postorder iterative.
 
-  def levelorderIterativ(self):
-      from collections import deque
-      queue = deque([])  # Wir benutzen einmal den Python eigene Queue, eine deque(Liste). ;-)
-      
-      queue.append(self._head._right)
-      
-      while queue:
-          n = queue.popleft()
-          self.printnode(n)
-          if n._left is not self._sentinal:
-              queue.append(n._left)
-          if n._right is not self._sentinal:
-              queue.append(n._right)
-          
+        Postorder Traversierung des Baumes mittels Iteration und eines Stacks.
+        """
+        stack = []  # Wir benutzen den Python eigenen Stack: eine Liste.
 
-  def printnode(self, node):
-    print("["+str(node._key)+": "+str(node._info), end="] " ,flush=True)
-    
+        n = self._head._right
+
+        done = False
+
+        while not done:
+
+            while (n is not self._sentinal):
+                if n._right is not self._sentinal:
+                    stack.append(n._right)
+                stack.append(n)
+                n = n._left
+
+            n = stack.pop()
+
+            if (n._right is not self._sentinal
+               and stack
+               and stack[-1] is n._right):
+                stack.pop()
+                stack.append(n)
+                n = n._right
+            else:
+                self.printnode(n)
+                n = self._sentinal
+            if not stack:
+                done = True
+
+    def levelorderIterativ(self):
+        """Traverse tree in levelorder iterative.
+
+        Inorder Traversierung des Baumes mittels Iteration und eines Stacks.
+        """
+        from collections import deque
+        queue = deque([])  # Wir benutzen die Python eigene Queue, eine deque.
+
+        queue.append(self._head._right)
+
+        while queue:
+            n = queue.popleft()
+            self.printnode(n)
+            if n._left is not self._sentinal:
+                queue.append(n._left)
+            if n._right is not self._sentinal:
+                queue.append(n._right)
+
+    def printnode(self, node):
+        """Print node information.
+
+        Gib die aktuelle Node (Schlüssel und Infromation) aus.
+        """
+        print(str(node), end=" ", flush=True)
+
+# ============================================================================
 
 
 def main():
-  tree = Tree()
-  tree.insert(20, "20")
-  tree.insert(10, "10")
-  tree.insert(5, "5")
-  tree.insert(15, "15")
-  tree.insert(30, "30")
-  tree.insert(25, "25")
-  tree.insert(35, "35")
-  '''
-  Der aufgebaute Baum:
-  
+    tree = Tree()
+    tree.insert(20, "20")
+    tree.insert(10, "10")
+    tree.insert(5, "5")
+    tree.insert(15, "15")
+    tree.insert(30, "30")
+    tree.insert(25, "25")
+    tree.insert(35, "35")
+
+    '''
+    Der aufgebaute Baum:
+
                  [20]
-                 
+
         [10]               [30]
-        
+
     [ 5]    [15]       [25]    [35]
-  
-  '''
 
-  print("\nRekursive Traversierung:")
-  print("Preorder:")
-  tree.preorderRecursiv(tree._head._right)
-  print("\nInorder:")
-  tree.inorderRecursiv(tree._head._right)
-  print("\nPostorder:")
-  tree.postorderRecursiv(tree._head._right)
+    '''
 
-  print("\n\nIterative Traversierung:")
-  print("Preorder:")
-  tree.preorderIterativ()
-  print("\nInorder:")
-  tree.inorderIterativ()
-  print("\nPostorder:")
-  tree.postorderIterativ()
-  print("\nLevelorder:")
-  tree.levelorderIterativ()
+    print("\nRekursive Traversierung:")
+    print("Preorder:")
+    tree.preorderRecursiv(tree._head._right)
+    print("\nInorder:")
+    tree.inorderRecursiv(tree._head._right)
+    print("\nPostorder:")
+    tree.postorderRecursiv(tree._head._right)
 
-  print("")
+    print("\n\nIterative Traversierung:")
+    print("Preorder:")
+    tree.preorderIterativ()
+    print("\nInorder:")
+    tree.inorderIterativ()
+    print("\nPostorder:")
+    tree.postorderIterativ()
+    print("\nLevelorder:")
+    tree.levelorderIterativ()
+
+    print("")
+
 
 def test():
     from random import randint
-    
+
     tree = Tree()
     n = 1000
-    
+
     list = [0] * n
     for i in range(0, n):
         list[i] = randint(0, n**2)
-        
+
     for value in list:
         tree.insert(value, str(value))
 
@@ -241,6 +322,7 @@ def test():
     print("")
     tree.inorderRecursiv(tree._head._right)
     print("")
+
 
 if __name__ == "__main__":
     main()
