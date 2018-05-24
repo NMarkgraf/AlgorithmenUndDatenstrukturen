48b740f251b8045af01e96c5ce269eaa4f352998
Norman Markgraf <NMarkgraf@users.noreply.github.com>
2018-05-24 16:06:05 +0200

    Kleinere Änderungen.

--- python/ackermann.py
+++ python/ackermann.py
@@ -35,6 +35,42 @@ def ackermann2(m, n):
         m -= 1
     return n + 1
     
+a={}
+    
+def memo(m, n):
+  if m == 0:
+      return n+1
+  if (m, n) in a:
+      return a[(m, n)]
+  else:
+      return None
+    
+    
+def ackermannMemo(m, n):
+    # print(a)
+    print(m,n, flush=True)
+    if m == 0:
+        return n + 1
+    else:
+        if n == 0:
+            if (m-1, 1) not in a:
+              a[(m-1, 1)] = ackermannMemo(m-1, 1)
+            return a[(m-1, 1)]
+        else:
+            t = memo(m, n-1)
+            
+            if not t:
+              t = ackermannMemo(m, n-1)
+              a[(m, n-1)] = t 
+            
+            tt = memo(m-1, t)
+            
+            if not tt:
+              tt = ackermannMemo(m-1, t)
+              a[(m-1, t)] = tt
+              
+            return tt
+
 
 def ackermannIterative(m, n):
     stack = []
@@ -42,11 +78,11 @@ def ackermannIterative(m, n):
     while stack:
         m = stack.pop()
         if m == 0:
-            n += m+1
+            n += 1
         else:
             if n == 0:
                 n = 1
-                m = m - 1
+                m -= 1
                 stack.append(m)
             else:
                 stack.append(m-1)
@@ -68,6 +104,7 @@ def sudan(n, x, y):
         else:
             return sudan(n-1, sudan(n,x, y-1), sudan(n,x,y-1) + y)
 
-print(ackermannIterative(4, 1))
-
+#print(ackermannIterative(3, 1))
+#print(ackermann2(4,1))
+print(ackermannMemo(4,1))
 #print(sudan(2,2,2))
