f84acf1f89b3dfe8a1fd40fe350fee444fc7ccce
Norman Markgraf <nmarkgraf@hotmail.com>
2018-05-30 11:24:11 +0200

    Weitergehende Hinweise und Rabin-Karp hinzugefügt.

--- python/stringsearch.py
+++ python/stringsearch.py
@@ -14,6 +14,15 @@ Teilstringsuche -- Suchen in Zeichenketten
 def brute_force(pat: str, txt: str) -> int:
     """Rohe Gewalt.
 
+    Anzahl der Operationen:
+
+     Garantiert  |  Typisch
+    -------------+-----------
+     M * N       | 1,1 * N
+
+    Zusatzspeicher: 1
+
+
     :param pat: Suchmuster
     :param txt: Text in dem gesucht werden soll
     :return: Findeposition oder -1, wenn nicht vorhanden.
@@ -37,6 +46,14 @@ def brute_force(pat: str, txt: str) -> int:
 def knuth_morris_pratt(pat: str, txt: str) -> int:
     """Algorithmus von Knuth Morris und Pratt.
 
+    Anzahl der Operationen:
+
+     Garantiert  |  Typisch
+    -------------+-----------
+     2 * N       | 1,1 * N
+
+    Zusatzspeicher: M * R
+
     :param pat: Suchmuster
     :param txt: Text in dem gesucht werden soll
     :return: Findeposition oder -1, wenn nicht vorhanden.
@@ -53,13 +70,14 @@ def knuth_morris_pratt(pat: str, txt: str) -> int:
 
         #  Erzeuge einen leeren endlichen Automaten
         new_dfa = [[0 for row in range(0, m)] for col in range(0, r+1)]
-        new_dfa[ord(pat[0])][1] = 1
+        new_dfa[ord(pat[0])][0] = 1
         x = 0
         for j in range(1, m):
+            cj = ord(pat[j])
             for c in range(0, r):
                 new_dfa[c][j] = new_dfa[c][x]
-            new_dfa[ord(pat[j])][j] = j + 1
-            x = new_dfa[ord(pat[j])][x]
+            new_dfa[cj][j] = j + 1
+            x = new_dfa[cj][x]
         return new_dfa
 
     dfa = create_dfa(pat)
@@ -69,7 +87,7 @@ def knuth_morris_pratt(pat: str, txt: str) -> int:
     n = len(txt)
     m = len(pat)
     while i < n and j < m:
-        j = dfa[ord(txt[i])][j-1]
+        j = dfa[ord(txt[i])][j]
         i += 1
     if j == m:
         return i-m
@@ -80,6 +98,14 @@ def knuth_morris_pratt(pat: str, txt: str) -> int:
 
 def boyer_moore(pat: str, txt: str) -> int:
     """Algorithmus von Boyer und Moore.
+
+    Anzahl der Operationen:
+
+     Garantiert  |  Typisch
+    -------------+-----------
+     3 * N       | N / M
+
+    Zusatzspeicher: R
 
     :param pat: Suchmuster
     :param txt: Text in dem gesucht werden soll
@@ -100,7 +126,6 @@ def boyer_moore(pat: str, txt: str) -> int:
     right = create_right(pat)
     n = len(txt)
     m = len(pat)
-    skip = 1
     i = 0
     while i <= n-m:
         skip = 0
@@ -121,14 +146,92 @@ def boyer_moore(pat: str, txt: str) -> int:
 # ============================================================================
 
 
+def rabin_karp(pat: str, txt: str) -> int:
+    """Algorithmus von Rabin Karp (Fingerprint-Algorithmus).
+
+    Anzahl der Operationen:
+
+     Garantiert  |  Typisch
+    -------------+-----------
+     7 * N       | 7 * N
+
+    Zusatzspeicher: 1
+
+    :param pat: Suchmuster
+    :param txt: Text in dem gesucht werden soll
+    :return: Findeposition oder -1, wenn nicht vorhanden.
+    """
+
+    q = 8388593  # Primzahl / Modulus
+    r = 256
+
+    def get_inv(r: int, q: int, m: int) -> int:
+        """Erzeuge ein inverses Element.
+
+            $inv = r^(m-1) mod q$
+
+        :param r: Größe des Zeichenbereichs (ASCII=256)
+        :param q: (Primzahl-)Modulus
+        :param m: Länge des Suchmusters
+        """
+        inv = 1
+        for i in range(1,m):
+            inv = (inv * r) % q
+        return inv
+
+
+    def check(pat: str, ctxt: str):
+        """Prüfe ob pat wirklich gleich ctxt ist.
+
+        :param pat: Suchmuster
+        :param ctxt: Textausschnitt
+        :return: TRUE falls gleich, sonst FALSE
+        """
+        # Monte Carlo: einfach nur TRUE liefern!
+        return True
+        # Las Vegas: Wirklich testen!
+        # return pat == ctxt
+
+
+    def hash(key: str, r: int, q: int) -> int:
+        """Hashfunktion.
+
+        :param key: Schlüssel
+        :param r: Größe des Zeichenbereichs
+        :param q: Modulus
+        :return:
+        """
+        h = 0
+        for i in range(len(key)):
+            h = (h * r + ord(key[i])) % q
+        return h
+
+    n = len(txt)
+    m = len(pat)
+    inv = get_inv(r, q, m)
+
+    pat_hash = hash(pat, r, q)
+    txt_hash = hash(txt[0:m], r, q)
+
+    i = m
+    while i < n:
+        if pat_hash == txt_hash:
+            if check(pat, txt[i-m:i]):
+                return i-m
+        txt_hash = (txt_hash + q - (inv*ord(txt[i-m]) % q)) % q
+        txt_hash = (txt_hash * r + ord(txt[i])) % q
+        i += 1
+    return -1
+
+
 def main():
     txt = "Das ist ein einfacher Text um zu Testen ob die Teilstringsuche funktioniert."
-    # txt = "1Teil"
     pat = "Teil"
     
-    print(brute_force(pat, txt))
-    print(knuth_morris_pratt(pat, txt))
-    print(boyer_moore(pat, txt))
+    print("Bruce force       :\t", brute_force(pat, txt))
+    print("Knuth-Morris-Pratt:\t", knuth_morris_pratt(pat, txt))
+    print("Boyer-Moore       :\t", boyer_moore(pat, txt))
+    print("Rabin-Karp        :\t", rabin_karp(pat, txt))
 
 if __name__ == "__main__":
     main()
